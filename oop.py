05478e3c6b9490414241b598d527f854f4522d0f
Naymur000 <nayeemur000@gmail.com>
2026-07-10 11:08:08 +0600

    1st commit

---
+++ oop.py
@@ -0,0 +1,11 @@
+class Car:
+    def __init__(self):
+        self.model = ""
+        self.brand = ""
+
+car1 = Car()
+car1.brand = "Toyota"
+car1.model = "Corolla"
+
+print(car1.brand)
+print(car1.model)
