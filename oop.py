f7f93f53b55db688980dfbac55254544f15f8695
Naymur000 <nayeemur000@gmail.com>
2026-07-11 11:41:38 +0600

    3rd

--- oop.py
+++ oop.py
@@ -1,18 +1,23 @@
 class Car:
-    def __init__(self):
-        self.model = ""
-        self.brand = ""
+    # def __init__(self):
+    #     self.model = ""
+    #     self.brand = ""
 
-car1 = Car()
-car1.brand = "Toyota"
-car1.model = "Corolla"
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+
+car1 = Car("Toyota", "Corolla")
+# car1.brand = "Toyota"
+# car1.model = "Corolla"
 
 print(car1.brand)
 print(car1.model)
 
-car2 = Car()
-car2.brand = "Honda"
-car2.model = "Civic"
+car2 = Car("Honda", "Civic")
+# car2.brand = "Honda"
+# car2.model = "Civic"
 
 print(car2.brand, car2.model)
 
